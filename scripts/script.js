91779789cbdea7ae44037b8630fb47c0bf25eae5
JulioVentorim <julioventorim@gmail.com>
2026-01-28 19:53:40 -0300

    funções 1.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
-function clique() {
-    let bot = document.querySelectorAll('.botao')
-    bot.forEach(addEventListener('click', clique())) {
-        console.log('')
-    }
-}
+const btn = document.querySelectorAll('.botao');
+const articles = document.querySelectorAll("articleclass");
+
+
+btn.forEach(function(btn){
+    btn.addEventListener("click", event => {
+        articles.forEach(article => article.style.display = "none");
+        const articleId = btn.dataset.articleid;
+        document.getElementById(articleId).style.display = "block";
+        console.log(event)
+    });
+});
