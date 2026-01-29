41ce4d3a61332152157511ca8811fa5b3ecc429c
JulioVentorim <julioventorim@gmail.com>
2026-01-29 19:21:24 -0300

    melhorias

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 const btn = document.querySelectorAll('.botao');
-const articles = document.querySelectorAll("articleclass");
+const articles = document.querySelectorAll(".divclass");
 
 
-btn.forEach(function(btn){
-    btn.addEventListener("click", event => {
+btn.forEach(function(clicarbtn) {
+    clicarbtn.addEventListener("click", event => {
         articles.forEach(article => article.style.display = "none");
-        const articleId = btn.dataset.articleid;
+        const articleId = clicarbtn.dataset.articleid;
         document.getElementById(articleId).style.display = "block";
         console.log(event)
     });
