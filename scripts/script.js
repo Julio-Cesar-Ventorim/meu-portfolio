02876681ca8e7f51f8041b19d2feb73f37b0fe00
JulioVentorim <julioventorim@gmail.com>
2026-02-14 21:49:52 -0300

    Portifólio v1.5

--- scripts/script.js
+++ scripts/script.js
@@ -11,14 +11,25 @@ btn.forEach(function(clicarbtn) {
     });
 });
 */
-       
+const pastas = document.querySelectorAll(".divclass");      
 const btn = document.querySelectorAll('.botao');
 
 btn.forEach(function(botao) {
     botao.addEventListener("click", event => {
         event.preventDefault()
-        const secaoId = botao.dataset.secaoid
-        const secao = document.getElementById(secaoId)
-        secao.scrollIntoView({behavior: "smooth"})
-    })
+        if(window.matchMedia("(min-width: 901px)").matches) {
+            pastas.forEach(conteudo => conteudo.style.display = "none");
+            const pastaId = botao.dataset.secaoid;
+            if(pastaId === "home") {
+                document.getElementById("homepc").style.display = "block";
+            }
+            document.getElementById(pastaId).style.display = "block";
+            console.log(event)
+        } else {
+            const secaoId = botao.dataset.secaoid
+            const secao = document.getElementById(secaoId)
+            secao.scrollIntoView({behavior: "smooth"})
+        }
+
+    });
 });
